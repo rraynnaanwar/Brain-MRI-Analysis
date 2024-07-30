e53cfc59fb26c1e7903e89a6fdb110cc4dcb438f
rraynaanwar <rraynaanwar@gmail.com>
2024-07-29 23:16:19 -0400

    created neural network class unfinished

---
+++ neuralNetwork/neuralNetwork.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import timm
+    
+class BrainMRIClassifier(nn.Module):
+    #where we define all the parts of the model
+    def __init__(self, numClasses=2):
+        
